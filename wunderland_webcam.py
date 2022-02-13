4bd454fcb86d3b4ebcd97ca4b25989a825476508
DrHaid <40427630+DrHaid@users.noreply.github.com>
2022-02-13 12:38:27 +0100

    added animated wunderland to webcam

--- wunderland_webcam.py
+++ wunderland_webcam.py
@@ -1,9 +1,12 @@
 import os
+import time
 import cv2
 import numpy as np
 import argparse
 import pyvirtualcam
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
+
+from wunderland import Wunderland
 
 
 def threshold(img, thresh=128, maxval=255, type=cv2.THRESH_BINARY):
@@ -11,6 +14,12 @@ def threshold(img, thresh=128, maxval=255, type=cv2.THRESH_BINARY):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     threshed = cv2.threshold(img, thresh, maxval, type)[1]
     return threshed
+
+def place_images(wunderland: Wunderland, img_name: str, count: int):
+    for x in range(0, count):
+        img = wunderland.get_image_from_name(img_name)
+        pos = wunderland.get_random_position(True)
+        wunderland.add_entity(wunderland=wunderland, image=img, position=pos, facing_right=(x % 2 == 0))  
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,34 +35,35 @@ def main():
     cap.set(cv2.CAP_PROP_FPS, fps)
     segmentor = SelfiSegmentation()
 
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    bg_img = cv2.imread(f'{ROOT_DIR}/img/wallpaper_partlycloudy.png')
-    bg_img = cv2.resize(bg_img, (width, height))
+    wunderland = Wunderland()
+    # place cows
+    place_images(wunderland=wunderland, img_name="cow", count=6)
 
     with pyvirtualcam.Camera(width=width, height=height, fps=fps) as cam:
         print(f'Using virtual camera: {cam.device}')
+        last_time = time.time()
         while True:
             success, frame = cap.read()
             img_out = segmentor.removeBG(frame, imgBg=(0, 255, 0), threshold=0.73)  # replace background with green
             green = np.array([0, 255, 0])
             mask = cv2.inRange(img_out, green, green) # make mask using green background
             
-            # # magic for mask smoothing and hole filling (rubbish)
-            # mask = cv2.bitwise_not(mask)
-            # kernel = np.ones((20, 20), 'uint8')
-            # mask1 = cv2.dilate(mask, kernel=kernel, iterations=1)
-            # mask1 = cv2.erode(mask1, kernel=kernel, iterations=1)
-            # mask = cv2.bitwise_not(mask)
-            # mask_blurred = cv2.GaussianBlur(mask, (27, 27), 0)
-            
             # smooth mask outlines (needs work)
             mask  = cv2.GaussianBlur(mask, (27, 27), 0)
             mask = threshold(mask)
 
-            # combine frame and bg_img
+            # get next wunderland rendered frame
+            delta_time = time.time() - last_time
+            wunderland.do_animation_step(delta_time=delta_time)
+            last_time = time.time()
+            wunderland_frame = wunderland.get_frame()
+            wunderland_frame = cv2.cvtColor(np.array(wunderland_frame), cv2.COLOR_RGB2BGR)
+            wunderland_frame = cv2.resize(wunderland_frame, (width, height))
+
+            # combine frame and wunderland_frame
             res = cv2.bitwise_and(frame, frame, mask=mask)
             f = frame - res
-            f = np.where(f == 0, bg_img, f)
+            f = np.where(f == 0, wunderland_frame, f)
 
             cam.send(f)
             cam.sleep_until_next_frame()
